fe43390d3741b0691a226a45f7d921a4812e90ad
LeeSeongGyu <lsg9706@naver.com>
2024-06-14 23:59:53 +0900

    2024-06-14

--- 백준/완전탐색/P17142_연구소3/Main.java
+++ 백준/완전탐색/P17142_연구소3/Main.java
@@ -5,129 +5,139 @@ import java.util.*;
 
 public class Main {
 
-	static int[] xMoves = {-1, 1, 0, 0};
-	static int[] yMoves = {0, 0, 1, -1};
-	static int n, m;
-	static int min_time = Integer.MAX_VALUE;
-	static int[][] map, copy;
-	static List<int[]> candidates = new ArrayList<>();
-	static int[] select;
+    static int[] xMoves = {-1, 1, 0, 0};
+    static int[] yMoves = {0, 0, 1, -1};
+    static int n, m;
+    static int min_time = Integer.MAX_VALUE;
+    static int[][] map, copy, visited;
+    static List<int[]> candidates = new ArrayList<>();
+    static int[] select;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
-		map = new int[n][n];
-		copy = new int[n][n];
-		select = new int[m];
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        map = new int[n][n];
+        copy = new int[n][n];
+        visited = new int[n][n];
+        select = new int[m];
 
-		for (int i = 0; i < n; i++) {
-			st = new StringTokenizer(br.readLine(), " ");
-			for (int j = 0; j < n; j++) {
-				map[i][j] = Integer.parseInt(st.nextToken());
-				if (map[i][j] == 2) {
-					candidates.add(new int[]{i, j});
-				}
-			}
-		}
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            for (int j = 0; j < n; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+                if (map[i][j] == 2) {
+                    candidates.add(new int[]{i, j});
+                }
+            }
+        }
 
-		DFS(0, 0);
-		if (min_time == Integer.MAX_VALUE) {
-			bw.write("" + -1);
-		} else {
-			bw.write("" + min_time);
-		}
-		bw.flush();
-		bw.close();
-	}
+        DFS(0, 0);
+        if (min_time == Integer.MAX_VALUE) {
+            bw.write("" + -1);
+        } else {
+            bw.write("" + min_time);
+        }
+        bw.flush();
+        bw.close();
+    }
 
-	public static void DFS(int depth, int index) {
-		if (depth == m) {
-			initCopy();
-			for (int i : select) {
-				int[] point = candidates.get(i);
-				copy[point[0]][point[1]] = 1;
-			}
-			getInfectionTime();
-			return;
-		}
+    public static void DFS(int depth, int index) {
+        if (depth == m) {
+            BFS();
+            return;
+        }
 
-		for (int i = index; i < candidates.size(); i++) {
-			select[depth] = i;
-			DFS(depth + 1, i + 1);
-		}
-	}
+        for (int i = index; i < candidates.size(); i++) {
+            select[depth] = i;
+            DFS(depth + 1, i + 1);
+        }
+    }
 
-	public static void getInfectionTime() {
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < n; j++) {
-				if (copy[i][j] == 1) {
-					infect(1, i, j);
-				}
-			}
-		}
+    public static void BFS() {
+        initCopy();
+        initVisited();
 
-		if(noBlank()){
-			min_time = Math.min(min_time, getMaxTime());
-		}
-	}
+        Queue<int[]> queue = new LinkedList<>();
 
-	public static void infect(int time, int x, int y) {
-		for (int i = 0; i < 4; i++) {
-			int nx = x + xMoves[i];
-			int ny = y + yMoves[i];
+        for (int i : select) {
+            int[] point = candidates.get(i);
+            copy[point[0]][point[1]] = 0;
+            visited[point[0]][point[1]] = 1;
+            queue.offer(point);
+        }
 
-			if (nx < 0 || nx >= n || ny < 0 || ny >= n || copy[nx][ny] == -1)  {
-				continue;
-			}
+        int time = 1;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
 
-			if (copy[nx][ny] == 0 || copy[nx][ny] == -2 || time + 1 < copy[nx][ny]) {
-				copy[nx][ny] = time + 1;
-				infect(time + 1, nx, ny);
-			}
-		}
-	}
+            for (int i = 0; i < size; i++) {
+                int[] poll = queue.poll();
 
-	public static void initCopy() {
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < n; j++) {
-				if (map[i][j] == 1) {
-					copy[i][j] = -1;
-				} else if (map[i][j] == 2) {
-					copy[i][j] = -2;
-				} else {
-					copy[i][j] = 0;
-				}
-			}
-		}
-	}
+                for (int j = 0; j < 4; j++) {
+                    int nx = poll[0] + xMoves[j];
+                    int ny = poll[1] + yMoves[j];
 
-	public static boolean noBlank() {
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < n; j++) {
-				if (copy[i][j] == 0) {
-					return false;
-				}
-			}
-		}
-		return true;
-	}
+                    if (nx >= 0 && nx < n && ny >= 0 && ny < n && visited[nx][ny] == 0
+                        && copy[nx][ny] != -2) {
+                        queue.offer(new int[]{nx, ny});
+                        copy[nx][ny] = time;
+                        visited[nx][ny] = 1;
+                    }
+                }
+            }
 
-	public static int getMaxTime(){
-		int max = Integer.MIN_VALUE;
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < n; j++) {
-				if(map[i][j] != 2 && map[i][j] != 1){
-					max = Math.max(max, copy[i][j]);
-				}
-			}
-		}
-		if(max == Integer.MIN_VALUE){
-			return 0;
-		}
-		return max - 1;
-	}
+            time++;
+        }
+
+        if (noBlank()) {
+            min_time = Math.min(min_time, getMaxTime());
+        }
+    }
+
+    public static void initCopy() {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (map[i][j] == 1) {
+                    copy[i][j] = -2;
+                } else if (map[i][j] == 2) {
+                    copy[i][j] = -3;
+                } else {
+                    copy[i][j] = -1;
+                }
+            }
+        }
+    }
+
+    public static void initVisited() {
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(visited[i], 0);
+        }
+    }
+
+    public static boolean noBlank() {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (copy[i][j] == -1) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public static int getMaxTime() {
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (map[i][j] != 2 && map[i][j] != 1) {
+                    max = Math.max(max, copy[i][j]);
+                }
+            }
+        }
+        return max == Integer.MIN_VALUE ? 0 : max;
+    }
 }
---
+++ 백준/완전탐색/P3055_탈출/Main.java
@@ -0,0 +1,97 @@
+package 백준.완전탐색.P3055_탈출;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int r, c;
+    static char[][] map;
+    static int[][] visited;
+    static int[] xMoves = {-1, 1, 0, 0};
+    static int[] yMoves = {0, 0, 1, -1};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        r = Integer.parseInt(st.nextToken());
+        c = Integer.parseInt(st.nextToken());
+        map = new char[r][c];
+        visited = new int[r][c];
+
+        for (int i = 0; i < r; i++) {
+            String str = br.readLine();
+            for (int j = 0; j < c; j++) {
+                map[i][j] = str.charAt(j);
+            }
+        }
+
+        bw.write(BFS());
+        bw.flush();
+        bw.close();
+    }
+
+    public static String BFS() {
+        Queue<int[]> water_queue = new LinkedList<>();
+        Queue<int[]> squirrel_queue = new LinkedList<>();
+
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                if (map[i][j] == '*') {
+                    water_queue.offer(new int[]{i, j});
+                }
+                if(map[i][j] == 'S'){
+                    squirrel_queue.offer(new int[]{i, j});
+                    visited[i][j] = 1;
+                    map[i][j] = '.';
+                }
+            }
+        }
+
+        int time = 1;
+
+        while (!squirrel_queue.isEmpty()) {
+            // 물 이동
+            int size = water_queue.size();
+            for (int i = 0; i < size; i++) {
+                int[] poll = water_queue.poll();
+
+                for (int j = 0; j < 4; j++) {
+                    int nx = poll[0] + xMoves[j];
+                    int ny = poll[1] + yMoves[j];
+                    if (nx >= 0 && nx < r && ny >= 0 && ny < c && map[nx][ny] == '.') {
+                        map[nx][ny] = '*';
+                        water_queue.offer(new int[]{nx, ny});
+                    }
+                }
+            }
+
+            // 다람쥐 이동
+            size = squirrel_queue.size();
+            for (int i = 0; i < size; i++) {
+                int[] poll = squirrel_queue.poll();
+
+                for (int j = 0; j < 4; j++) {
+                    int nx = poll[0] + xMoves[j];
+                    int ny = poll[1] + yMoves[j];
+                    if(nx < 0 || nx >= r || ny < 0 || ny >= c || visited[nx][ny] == 1){
+                        continue;
+                    }
+                    if(map[nx][ny] == 'D'){
+                        return String.valueOf(time);
+                    }
+                    if (map[nx][ny] == '.') {
+                        squirrel_queue.offer(new int[]{nx, ny});
+                        visited[nx][ny] = 1;
+                    }
+                }
+            }
+
+            time++;
+        }
+
+        return "KAKTUS";
+    }
+}
