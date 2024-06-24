7fcba405746367fb6664ca31842c120c381b34c3
LeeSeongGyu <lsg9706@naver.com>
2024-06-24 20:15:48 +0900

    2024-06-24

---
+++ 백준/다이나믹프로그래밍/P9095_123만들기/Main.java
@@ -0,0 +1,42 @@
+package 백준.다이나믹프로그래밍.P9095_123만들기;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int T, n;
+    static int[] dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        T = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < T; i++){
+            n = Integer.parseInt(br.readLine());
+            dp = new int[n + 1];
+            bw.write(solution() + "\n");
+        }
+
+        bw.flush();
+        bw.close();
+    }
+
+    public static int solution(){
+        if(n == 1) return 1;
+        if(n == 2) return 2;
+        if(n == 3) return 4;
+
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 4;
+
+        for(int i = 4; i <= n; i++){
+            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+        }
+
+        return dp[n];
+    }
+}
